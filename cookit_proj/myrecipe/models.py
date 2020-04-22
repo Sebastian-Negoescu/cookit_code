a55aa849c0c8d12ab665ecb5b59f3a782f0681ca
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-04-22 18:06:09 +0300

    testing slug

--- cookit_proj/myrecipe/models.py
+++ cookit_proj/myrecipe/models.py
@@ -1,12 +1,20 @@
 from django.db import models
-
+from django_extensions.db.fields import AutoSlugField
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=150)
+    slug = AutoSlugField(_('slug'), populate_from='name', unique=True)
     description = models.TextField(max_length=3000)
 
+    def __unicode__(self):
+        return self.name
 
-    def __str__(self):
-        return f"{self.name}"
+    
+    def get_absolute_uri(self):
+        return f"/myrecipe/{self.slug}/"
+
+
+    
 
 # TO DO: besides the Recipe/Retete model, add "ingrediente" as one field with list "items property" && "images" for various photos that are to be uploaded for representation
--- cookit_proj/myrecipe/views.py
+++ cookit_proj/myrecipe/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import Http404
 from .models import Recipe
@@ -12,3 +13,4 @@ def home(request):
 def recipe_detail(request, id):
     recipe = Recipe.objects.get(id=id)
     return render(request, "myrecipe/recipe_detail.html", {"recipe": recipe})
+
