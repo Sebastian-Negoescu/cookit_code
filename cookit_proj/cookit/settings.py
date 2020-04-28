baf74bc291debbfb5a8b9e47566ddd46198d988f
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-04-28 15:36:48 +0300

    explicit url

--- cookit_proj/cookit/settings.py
+++ cookit_proj/cookit/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '9j^#_f-(c*u2)y=3#tw!)(kxf93@!l6)uyhq(w+xsm&o!o*j85'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cookit-webapp-dev.azurewebsites.net']
-
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1", "cookit-webapp-dev.azurewebsites.net"]
+ALLOWED_HOSTS = ['cookit-webapp-dev.azurewebsites.net']
 # Application definition
 
 INSTALLED_APPS = [
