8facde6ac07e3c6ee96c92cbe75bdaf37aabce8c
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-04-30 18:42:33 +0300

    test final dev changes

--- cookit_proj/post_build.sh
+++ cookit_proj/post_build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+gunicorn --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot cookit.wsgi
+cd /home/site/wwwroot
+source /antenv/bin/activate
+python3 -m pip install -r requirements.txt
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DBUSER', 'sebastian.negoescu@gmail.com', '$DBPASS')" | python3 manage.py shell
 python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
+python3 manage.py migrate --run-syncdb
