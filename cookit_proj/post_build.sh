5e417c1d113451bd131f523c80b3e9aef1063532
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-05-04 14:50:36 +0300

    add workers && thread in gunicorn

--- cookit_proj/post_build.sh
+++ cookit_proj/post_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot cookit.wsgi # let just this one as StartupCommand
+gunicorn --workers 8 --threads 4 --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot cookit.wsgi # let just this one as StartupCommand
 # cd /home/site/wwwroot
 # source /antenv/bin/activate
-# python3 -m pip install --upgrate -r requirements.txt
+# python3 -m pip install --upgrade -r requirements.txt
 # python3 manage.py makemigrations
 # python3 manage.py migrate
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DBUSER', 'sebastian.negoescu@gmail.com', '$DBPASS')" | python3 manage.py shell
