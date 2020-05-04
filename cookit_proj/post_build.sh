d9e1ecdcff32605e4c6833e5545cf0706eaff8fe
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-05-04 15:14:08 +0300

    retry full shell script

--- cookit_proj/post_build.sh
+++ cookit_proj/post_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 gunicorn --workers 8 --threads 4 --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot cookit.wsgi # let just this one as StartupCommand
-# cd /home/site/wwwroot
-# source /antenv/bin/activate
-# python3 -m pip install --upgrade -r requirements.txt
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-# echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DBUSER', 'sebastian.negoescu@gmail.com', '$DBPASS')" | python3 manage.py shell
-# python3 manage.py migrate --run-syncdb
+cd /home/site/wwwroot
+source /antenv/bin/activate
+python3 -m pip install --upgrade -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DBUSER', 'sebastian.negoescu@gmail.com', '$DBPASS')" | python3 manage.py shell
+python3 manage.py migrate --run-syncdb
